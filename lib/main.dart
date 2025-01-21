ba7747bdf78260f4f21af48c7b3b552983cdcdf7
Prisha <prisha09@cs.washington.edu>
2025-01-21 13:31:04 -0800

    started project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +10,55 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return SafeArea(
+      child: CupertinoApp(
+        home: CupertinoTabScaffold(
+          tabBar: CupertinoTabBar(items: [
+            BottomNavigationBarItem(icon: Icon(Icons.auto_awesome), label: 'Suggestions'),
+            BottomNavigationBarItem(icon: Icon(Icons.checkroom), label: 'Closet')
+          ]),
+          tabBuilder: (BuildContext context, int index){
+            if(index == 0){
+              return Suggestions();
+            } else {
+              return Closet();
+            }
+            return Placeholder();
+          },
         ),
       ),
     );
   }
 }
+
+class Suggestions extends StatelessWidget {
+  const Suggestions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+          children: [
+            //Text('Stich', style: GoogleFonts.miniver(textStyle: TextStyle(fontSize: 30))),
+            const Spacer()
+          ],),
+        ), 
+        const Spacer()
+      ],
+    );
+  }
+}
+
+
+class Closet extends StatelessWidget {
+  const Closet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
